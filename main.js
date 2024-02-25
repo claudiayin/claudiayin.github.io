819a95f5ab10ae966475d0a74f373fefed04dc55
claudiayin <20230052@mywhitecliffe.com>
2024-02-25 15:57:15 +1300

    update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.addEventListener(("scroll"),()=>{
-    document.querySelector("#Home h1").style.marginTop = `${window.scrollY * 1.5}px`
+    document.querySelector("#LogoCloud").style.marginTop = `${window.scrollY * 1.5}px`
     document.querySelector("#Mountain1").style.marginBottom = `${106 - window.scrollY}px`
     document.querySelector("#leftCloud").style.marginLeft = `-${window.scrollY}px`
     document.querySelector("#mainCloud").style.marginTop = `-${window.scrollY}px`
